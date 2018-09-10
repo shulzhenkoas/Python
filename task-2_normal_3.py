14cd07b6b311de4d5b9099898b91498a272505e0
shulzhenkoas <shulzhenkoas@gmail.com>
2018-09-10 15:12:59 +0300

    Лекция 2 задание 3 (normal)

--- task-2_normal_3.py
+++ task-2_normal_3.py
@@ -6,20 +6,12 @@
 # Подсказка:
 # для получения случайного числа используйте функцию randint() модуля random
 
-print('Решим уравнение ax^2 + bx + c = 0')
-a = float(input("Введите коэффициент a: "))
-b = float(input("Введите коэффициент b: "))
-c = float(input("Введите коэффициент c: "))
-
-discr = b**2 - 4 * a * c;
-print("Дискриминант D = %.2f" % discr)
-if discr > 0:
-	import math
-	x1 = (-b + math.sqrt(discr)) / (2 * a)
-	x2 = (-b - math.sqrt(discr)) / (2 * a)
-	print("x1 = %.2f \nx2 = %.2f" % (x1, x2))
-elif discr == 0:
-	x = -b / (2 * a)
-	print("x = %.2f" % x)
-else:
-	print("Корней нет")
+from random import randint
+sequence3 = []
+range = randint(0, 30)
+i = 0
+while i < range:
+    sequence3.append(randint(-100, 100))
+    i += 1
+print("В списке ниже {} элементов:".format(range))
+print(sequence3)
