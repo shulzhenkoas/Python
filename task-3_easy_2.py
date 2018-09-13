92e6ce01d381e33a96aa9fe9b32466d8feb1c6ca
shulzhenkoas <shulzhenkoas@gmail.com>
2018-09-13 15:22:26 +0300

    Лекция 3 задание 2 (easy)

--- task-3_easy_2.py
+++ task-3_easy_2.py
@@ -8,9 +8,26 @@
 # !!!P.S.: функция не должна НИЧЕГО print'ить
 
 def lucky_ticket(ticket_number):
-    pass
+    string_ticket = str(ticket_number)
+    len_half = len(string_ticket) // 2
+    str1 = string_ticket[:len_half:]
+    str2 = string_ticket[:0-len_half-1:-1]
+    sum1 = 0
+    sum2 = 0
+    for a in str1:
+        sum1 += int(a)
 
+    for b in str2:
+        sum2 += int(b)
 
+    if sum1 == sum2:
+        return 'Билет счастливый!'
+    else:
+        return 'Билет не счастливый :('
+
+print("P.S.: При нечетном количестве цифр, цифра по середине опускается")
 print(lucky_ticket(123006))
 print(lucky_ticket(12321))
 print(lucky_ticket(436751))
+print(lucky_ticket(4367518))
+print(lucky_ticket(1234567890987654321))
