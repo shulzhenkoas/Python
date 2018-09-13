721e7f813bcdbcbd108118fbf336cfd409201db4
shulzhenkoas <shulzhenkoas@gmail.com>
2018-09-13 17:15:20 +0300

    Лекция 3 задание 3 (normal)

--- task-3_normal_3.py
+++ task-3_normal_3.py
@@ -4,3 +4,26 @@
 # Задача-3:
 # Напишите собственную реализацию стандартной функции filter.
 # Разумеется, внутри нельзя использовать саму функцию filter.
+
+def func(unit):
+    if unit > 0:
+        return True
+    else:
+        return False
+
+
+def new_filter(func_name, *args):
+    new_args = []
+    for value in args:
+        if func_name(value):
+            new_args.append(value)
+    return new_args
+
+digs = [-3, -1, 0, 3, 7, -5]
+print(new_filter(func, *digs))
+
+# Просьба пояснить почему вот это работает:
+# print(new_filter(func, *[-3, -1, 0, 3, 7, -5]))
+# а вот это нет:
+# print(new_filter(func, [-3, -1, 0, 3, 7, -5]))
+# Спасибо.
