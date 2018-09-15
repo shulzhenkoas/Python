1f57f1926dc5da831010d1689c1b79efbcaff2f3
shulzhenkoas <shulzhenkoas@gmail.com>
2018-09-16 02:01:50 +0300

    Лекция 4 задание 1 (normal)

--- task-4_normal_1.py
+++ task-4_normal_1.py
@@ -1,13 +1,15 @@
 #__author__ = 'Шульженко А.С.'
 # Coding: UTF-8
 
- Задание-1:
+# Задание-1:
 # Вывести символы в нижнем регистре, которые находятся вокруг
 # 1 или более символов в верхнем регистре.
 # Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
 # Решить задачу двумя способами: с помощью re и без.
 
-line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
+import re
+
+line1 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'GIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzK'\
        'TUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSAHqn'\
        'LxooisBDWuxIhyjJaXDYwdoVPnsllMngNlmkpYOlqXEFIxPqqqgAWdJsOvqppOfyIVjXa'\
@@ -21,19 +23,11 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'UJmGYJZPpRBFNLkqigxFkrRAppYRXeSCBxbGvqHmlsSZMWSVQyzenWoGxyGPvbnhWHuXB'\
        'qHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFa'\
        'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
-'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
+       'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
 
-def fibonacci(n, m):
-#    print(n)
-#    print(m)
-    str_fib = [1, 1]
-    i = 2
-    while i < m:
-        str_fib.append(str_fib[i-1] + str_fib[i-2])
-        i += 1
-    print(str_fib)
-    return str_fib[n-1:m] # Срез не включает последний элемент, поэтому 'm', а не 'm-1'
-
-print("P.S.: Нумерация элементов начинается с 1-го. Отрицательные индексы не вычисляются для ряда Фибоначчи.")
-print(fibonacci(10,20))
+pattern1 = re.compile(r'[a-z]+')
+print(pattern1)
+lst_new4 = pattern1.findall(line1)
+print(type(lst_new4))
+print(lst_new4)
 
