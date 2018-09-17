25ace07547f4b78c2653badf04b681df10c3bd50
shulzhenkoas <shulzhenkoas@gmail.com>
2018-09-17 14:18:57 +0300

    Лекция 4 задание 1 (normal)

--- task-4_normal_1.py
+++ task-4_normal_1.py
@@ -25,11 +25,26 @@ line_1 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'
          'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
          'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
 
+# Решение с помощью регулярных выражений
+print("Поиск с помощью регулярных выражений...")
 pattern1 = re.compile(r'([a-z]+)[A-Z]')
-print(pattern1)
-lst_new4 = (pattern1.findall(line_1))
-print(type(lst_new4))
-print(lst_new4)
+#print(pattern1)
+lst4_new1 = pattern1.findall(line_1)
+#print(type(lst4_new1))
+print(lst4_new1)
+
+# Решение с помощью методов строк
+print("Поиск с помощью методов строк...")
+elem1 = ""
+lst4_new2 = []
+for i in line_1:
+    if i.islower():
+        elem1 = ''.join([elem1, i])
+    elif i.isupper() and elem1:
+        lst4_new2.append(elem1)
+        elem1=""
+#print(type(lst4_new2))
+print(lst4_new2)
 
 # Подскажите пожалуйста замедляют ли группировка в () регулярные выражения и насколько
 # Спасибо
