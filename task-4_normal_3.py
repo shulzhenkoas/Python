ab0290a40f47eb50ec49e89fdeadc18ba11eead4
shulzhenkoas <shulzhenkoas@gmail.com>
2018-09-17 17:26:20 +0300

    Лекция 4 задание 3 (normal)

--- task-4_normal_3.py
+++ task-4_normal_3.py
@@ -8,25 +8,38 @@
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
 
-def func(unit):
-    if unit > 0:
-        return True
-    else:
-        return False
+import re
+from os import getcwd,path
+from random import randrange
 
+line_3 = ""
+# Длина строки может быть max 2501, но это не принципиально
+while len(line_3) < 2500:
+    line_3 = ''.join([line_3, str(randrange(0, 100))])
+# Можно и так, но повторяющиеся последовательности цифр будут короткими
+#for _ in range(0, 2500):
+#    line_3 = ''.join([line_3, str(randint(0, 9))])
 
-def new_filter(func_name, *args):
-    new_args = []
-    for value in args:
-        if func_name(value):
-            new_args.append(value)
-    return new_args
+print("Длина строки: ",len(line_3))
+print("Получившаяся строка...")
+print(line_3)
 
-digs = [-3, -1, 0, 3, 7, -5]
-print(new_filter(func, *digs))
+# Записываем строку (ровно 2500 символов) в новый файл и закрываем его
+path = path.join(getcwd(), "Big_num.txt")
+with open(path, 'w', encoding='UTF-8') as file_write:
+    file_write.write(line_3[:2500])
 
-# Просьба пояснить почему вот это работает:
-# print(new_filter(func, *[-3, -1, 0, 3, 7, -5]))
-# а вот это нет:
-# print(new_filter(func, [-3, -1, 0, 3, 7, -5]))
-# Спасибо.
+# Читаем данные из файла и находим самую длинную последовательность
+line_3 = ""
+print("\nПеременная в которую читаем из файла должна быть пустой:", line_3)
+with open(path, 'r', encoding='UTF-8') as file_read:
+    line_3 = file_read.read()
+print("\nПрочитанная строка...")
+print(line_3)
+print("Длина прочитанной строки:",len(line_3))
+lst6_new = []
+for i in range(0, 9):
+    pattern3 = ''.join([str(i), '+'])
+    lst6_new.append(int(max(re.findall(pattern3, line_3))))
+#print(lst6_new)
+print("Самая длинная последовательность: ",max(lst6_new))
