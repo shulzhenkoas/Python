a61524d4b6096f9d56896373c7388e91428479d3
shulzhenkoas <shulzhenkoas@gmail.com>
2018-09-20 17:01:21 +0300

    Лекция 5 задание 1 (normal)

--- task-5_normal_1.py
+++ task-5_normal_1.py
@@ -17,4 +17,56 @@
 # оформленные в виде соответствующих функций,
 # и импортированные в данный файл из easy.py
 
+import os
+import task5_easy as easy
+
+def change_dir():
+    try:
+        dir_name = input("Введите директорию для перехода:")
+        os.chdir(dir_name)
+        print("Текущая директория: '{}'".format(os.getcwd()))
+    except FileNotFoundError:
+        print("Невозможно перейти в директорию: '{}'".format(dir_name))
+    except Exception as e:
+        print(e.__class__)
+
+def read_dir():
+    dir_name = "."
+    easy.list_dir(dir_name)
+    print("---------------------------------------------\n")
+
+def remove_dir():
+    dir_name = input("Введите директорию для удаления:")
+    easy.rm_dir(dir_name)
+
+def create_dir():
+    dir_name = input("Введите директорию для создания:")
+    easy.make_dir(dir_name)
+
+def user_action(numb):
+    print("action = ", numb)
+    if numb == 1:
+        change_dir()
+    elif numb == 2:
+        read_dir()
+    elif numb == 3:
+        create_dir()
+    elif numb == 4:
+        remove_dir()
+
+def main_prog():
+    while True:
+        choice = int(input("Выберите пункт:\n"
+                           "1. Перейти в директорию\n"
+                           "2. Просмотреть содержимое текущей директории\n"
+                           "3. Создать директорию\n"
+                           "4. Удалить директорию\n"
+                           "0. Выход\n"
+                           "---------------------------------------------\n"
+                           "Ваш выбор: "))
+        if choice == 0:
+            break
+        else:
+            user_action(choice)
+main_prog()
 
