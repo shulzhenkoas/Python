7eaaa0d336313b77dbddf83daafa96495e3b66e4
shulzhenkoas <shulzhenkoas@gmail.com>
2018-09-20 15:46:23 +0300

    Лекция 5 задание 3 (easy)

--- task-5_easy_3.py
+++ task-5_easy_3.py
@@ -3,3 +3,20 @@
 
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+
+import shutil
+import sys
+
+#print(__file__)
+#print("sys.argv = ", sys.argv[0])
+file_path = sys.argv[0]
+
+def copy_file():
+    try:
+        print("Создаем копию файла {}\n".format(file_path))
+        shutil.copyfile(file_path, file_path + "_bkp")
+    except Exception as e:
+        print(e.__class__)
+
+# Вызов функции создающей копию данного файла
+copy_file()
