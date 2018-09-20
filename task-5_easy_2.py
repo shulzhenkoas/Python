519b99aca7068cf7b0178c404e2824e779459064
shulzhenkoas <shulzhenkoas@gmail.com>
2018-09-20 15:10:27 +0300

    Лекция 5 задание 2 (easy)

--- task-5_easy_2.py
+++ task-5_easy_2.py
@@ -3,3 +3,21 @@
 
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
+
+import os
+
+def list_dir():
+    if not dir_name:
+        print("Укажите имя директории.")
+        return
+    dir_path = os.path.join(os.getcwd(), dir_name)
+    try:
+        print("Вывод содержимого текущей директории...\n",dir_path)
+        for idx in os.listdir(dir_path):
+            print(idx)
+    except PermissionError:
+        print("Не хватает прав на чтение директории {}".format(dir_path))
+
+# Вызов функции вывода содержимого текущей директории
+dir_name = '.'
+list_dir()
